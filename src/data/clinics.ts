8adae2eaea4e3e79a79848cc249cff67bdb31541
Fernando Leite <144856681+Ecolebrilha@users.noreply.github.com>
2025-11-26 17:56:19 -0300

    Update clinic information and add new entries

--- src/data/clinics.ts
+++ src/data/clinics.ts
@@ -1,24 +1,69 @@
-import { Clinic } from '../types/clinic';
-
 export const clinics: Clinic[] = [
+    // --- Mesma rua: R. Lourdes Casé Pôrto, 51 ---
     {
-        name: 'Clínica Estética Garanhuns',
-        address: 'Rua das Flores, 123 - Centro, Garanhuns - PE',
-        instagram: 'https://instagram.com/clinicaesteticagaranhuns'
+        name: 'Clínica Dra Mayara Bezerra Estética Avançada Em Caruaru',
+        address: 'R. Lourdes Casé Pôrto, 51 - 11º andar | Sala 1103 - Maurício de Nassau, Caruaru - PE, 55012-075',
+        instagram: 'https://instagram.com/dramayarabezerra',
+        hours: '09:00 - 18:00'
     },
     {
-        name: 'Harmoniza Garanhuns',
-        address: 'Avenida Rui Barbosa, 456 - Heliópolis, Garanhuns - PE',
-        instagram: 'https://instagram.com/harmonizagaranhuns'
+        name: 'Dra Laisy Velar | Harmonização Facial',
+        address: 'R. Lourdes Casé, 51 - 10° Andar, Sala 1019 - Maurício de Nassau, Caruaru - PE, 55012-700',
+        instagram: 'https://instagram.com/dralaisyvelar',
+        hours: '24 horas'
     },
     {
-        name: 'Beleza Facial Garanhuns',
-        address: 'Praça da Bandeira, 789 - São José, Garanhuns - PE',
-        instagram: 'https://instagram.com/beleza_facial_garanhuns'
+        name: 'Dra Gessica Karla | Harmonização Facial em Caruaru',
+        address: 'Empresarial Times Busines Center - Rua Maria de Lourdes Casé Pôrto, 51 - 6º andar sala 617 - Maurício de Nassau, Caruaru - PE, 55012-075',
+        instagram: 'https://instagram.com/dragessicakarla',
+        hours: 'Não informado'
+    },
+
+    // --- Mesma avenida: Av. Agamenon Magalhães ---
+    {
+        name: 'Clínica BotoEsthetic - Caruaru',
+        address: 'Av. Agamenon Magalhães, 1190 - Maurício de Nassau, Caruaru - PE, 55012-290',
+        instagram: 'https://instagram.com/botoesthetic_caruaru',
+        hours: '09:00 - 19:00'
     },
     {
-        name: 'Estética e Saúde Garanhuns',
-        address: 'Rua do Comércio, 321 - Boa Vista, Garanhuns - PE',
-        instagram: 'https://instagram.com/esteticaesaudegaranhuns'
+        name: 'Dra Patrícia Feitosa',
+        address: 'Clínica Levitté, Trade Center - Av. Agamenon Magalhães, sala 208 - Maurício de Nassau, Caruaru - PE, 55012-560',
+        instagram: 'https://instagram.com/drapatriciafeitosa',
+        hours: '08:00 - 21:00'
     },
+
+    // --- Endereços próximos na região central ---
+    {
+        name: 'Dra Oziane Costa | Harmonização Facial - Caruaru',
+        address: 'Rua Padre Antônio Tomaz, n° 91 - Maurício de Nassau, Caruaru - PE, 55012-630',
+        instagram: 'https://instagram.com/draozianecosta',
+        hours: '09:00 - 20:00'
+    },
+    {
+        name: 'Dra Lívia Galvão - Harmonização Facial',
+        address: 'Av. Dr. Pedro Jordão - Maurício de Nassau, Caruaru - PE, 55012-640',
+        instagram: 'https://instagram.com/draliviagalvao',
+        hours: '12:30 - 18:00'
+    },
+    {
+        name: 'Nayana Guaraná - Harmonização Facial',
+        address: 'Av. Oswaldo Cruz, 217 - Maurício de Nassau, Caruaru - PE, 55012-040',
+        instagram: 'https://instagram.com/dranayanaguarana',
+        hours: '14:00 - 22:00'
+    },
+
+    // --- Pontos mais afastados ---
+    {
+        name: 'Dra Fabiana Reis | Harmonização Facial em Caruaru',
+        address: 'R. Saldanha Marinho, 945 - Sala 7 - Maurício de Nassau, Caruaru - PE, 55014-330',
+        instagram: 'https://instagram.com/drafabianareis',
+        hours: '09:00 - 17:00'
+    },
+    {
+        name: 'Dra Gisa Nunes - Harmonização Facial',
+        address: 'Rua Ana Maria da Silva Brasileirinho, 17 - Maurício de Nassau, Caruaru - PE, 55014-325',
+        instagram: 'https://instagram.com/gisanunnes',
+        hours: '09:00 - 17:00'
+    }
 ];
